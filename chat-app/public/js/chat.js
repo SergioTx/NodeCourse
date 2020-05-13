4c929b384357399015f8c210567444657b1786a1
Sergio Municio <smunicio_profit@bankinter.com>
2020-05-13 15:35:39 +0200

    160

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,13 +1,27 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', (message) => {
   console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
   event.preventDefault();
+  // disable form
+  $messageFormButton.setAttribute('disabled', 'disabled');
+
   const message = event.target.elements.message.value;
+  $messageFormInput.value = '';
+  $messageFormInput.focus();
+
   socket.emit('sendMessage', message, (error) => {
+    // enable form
+    $messageFormButton.removeAttribute('disabled');
+
     if (error) {
       return console.log(error);
     }
@@ -15,14 +29,17 @@ document.querySelector('#message-form').addEventListener('submit', (event) => {
   });
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
 
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
     socket.emit('sendLocation', { latitude, longitude }, () => {
+      $sendLocationButton.removeAttribute('disabled');
       console.log('Location shared!');
     });
   });
