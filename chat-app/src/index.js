62d3e008c92b87bcd40d661823eddcc10ed733f9
Sergio Municio <smunicio_profit@bankinter.com>
2020-05-13 13:11:47 +0200

    154

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -10,6 +14,10 @@ console.log(publicDirectoryPath);
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('New websocket connection');
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
